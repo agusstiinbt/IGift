5cbd6bee90a7d47b33d2bf34cf34edae745b3869
Aesposito <agusstiinbt@gmail.com>
2024-05-03 09:34:22 -0300

    Add project files.

---
+++ IGift/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using IGift.Server.Models;
+using IGift.Shared;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+
+namespace IGift.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        public DbSet<SuperHeroe> SuperHeroes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ IGift/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IGift.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
