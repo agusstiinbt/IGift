203e5fb9e45661c4d2c6a56b4d4dc3fbf352bfda
Aesposito <agusstiinbt@gmail.com>
2024-07-19 12:32:33 -0300

    Limpieza de código

--- IGift/Client/Pages/Peticiones.razor.cs
+++ IGift/Client/Pages/Peticiones.razor.cs
@@ -1,10 +1,4 @@
-﻿using Client.Infrastructure.Services.Requests;
-using IGift.Application.Features.Pedidos.Query;
-using IGift.Application.Responses.Pedidos;
-using Microsoft.AspNetCore.Components;
-using MudBlazor;
-
-namespace IGift.Client.Pages
+﻿namespace IGift.Client.Pages
 {
     public partial class Peticiones
     {
