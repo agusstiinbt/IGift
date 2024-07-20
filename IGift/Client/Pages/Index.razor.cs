1fda3efbb7a34c6bf459068a6c61d758b74dba37
Aesposito <agusstiinbt@gmail.com>
2024-07-20 12:03:16 -0300

    Mejoras en el mainlayout

--- IGift/Client/Pages/Index.razor.cs
+++ IGift/Client/Pages/Index.razor.cs
@@ -1,35 +1,7 @@
-﻿using Client.Infrastructure.Authentication;
-using Client.Infrastructure.Services.Identity.Users;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using MudBlazor;
-
-namespace IGift.Client.Pages
+﻿namespace IGift.Client.Pages
 {
     public partial class Index
     {
-        [Inject] private IUserManager? _userManager { get; set; }
-        [Inject] private AuthenticationStateProvider? _authenticationStateProvider { get; set; }
-
-        private string NombreUsuario { get; set; }
-
-        public async void HandleSubmit()
-        {
-            var result = await _userManager.GetUsersAsync();
-            if (!result.Succeeded)
-            {
-                _snack.Add(result.Messages.ToString(), Severity.Error);
-            }
-            _snack!.Add("Consulta exitosa");
-        }
-        protected override async Task OnInitializedAsync()
-        {
-            var state = await ((IGiftAuthenticationStateProvider)_authenticationStateProvider!).GetAuthenticationStateAsync();
-            var user = state.User;
-            if (state != null && user.Identity.IsAuthenticated)
-            {
-                NombreUsuario = state.User.Identity.Name;
-            }
-        }
+       
     }
 }
---
+++ IGift/Client/Shared/BarraHerramientas/BarraHerramientasPrincipal.razor.cs
@@ -0,0 +1,25 @@
+﻿using Client.Infrastructure.Services.Identity.Users;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using Client.Infrastructure.Authentication;
+
+namespace IGift.Client.Shared.BarraHerramientas
+{
+    public partial class BarraHerramientasPrincipal
+    {
+        [Inject] private IUserManager? _userManager { get; set; }
+        [Inject] private AuthenticationStateProvider? _authenticationStateProvider { get; set; }
+
+        private string NombreUsuario { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var state = await ((IGiftAuthenticationStateProvider)_authenticationStateProvider!).GetAuthenticationStateAsync();
+            var user = state.User;
+            if (state != null && user.Identity.IsAuthenticated)
+            {
+                NombreUsuario = state.User.Identity.Name;
+            }
+        }
+    }
+}
