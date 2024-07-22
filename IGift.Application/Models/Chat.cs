91bea84492691038ec3f3b42ce4d05b5b9934686
Aesposito <agusstiinbt@gmail.com>
2024-07-22 11:55:52 -0300

    Creación de clases para el chat

--- IGift.Application/Models/Chat.cs
+++ IGift.Application/Models/Chat.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IGift.Application.Interfaces.Chat;
 
 namespace IGift.Application.Models
 {
-    public class Chat
+    public class Chat<TUser> : IChatHistory<TUser> where TUser : IChatUser
     {
+        public long Id { get; set; }
+        public string FromUserId { get; set; }
+        public string ToUserId { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public virtual TUser FromUser { get; set; }
+        public virtual TUser ToUser { get; set; }
     }
 }
