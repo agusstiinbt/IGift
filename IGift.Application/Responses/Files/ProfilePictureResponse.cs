115faaa4a0402118e50ff54abf0ac2934b8519f8
Aesposito <agusstiinbt@gmail.com>
2024-07-25 09:25:21 -0300

    Creamos una clase para poder traernos archivos desde el servidor

---
+++ IGift.Application/Responses/Files/ProfilePictureResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace IGift.Application.Responses.Files
+{
+    public class ProfilePictureResponse
+    {
+        public string FileName { get; set; } = string.Empty;
+        public string FileType { get; set; } = string.Empty;
+        public byte[] Data { get; set; }
+
+    }
+}
