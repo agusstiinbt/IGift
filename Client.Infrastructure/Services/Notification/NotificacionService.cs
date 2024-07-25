cc8710d669d6e25e54034f0210f90ee5a2a23895
Aesposito <agusstiinbt@gmail.com>
2024-07-25 13:44:16 -0300

    .

--- Client.Infrastructure/Services/Notification/NotificacionService.cs
+++ Client.Infrastructure/Services/Notification/NotificacionService.cs
@@ -22,7 +22,7 @@ namespace Client.Infrastructure.Services.Notification
 
         public async Task<IResult<IEnumerable<NotificationResponse>>> GetAll()
         {
-            //TODO implementar lógica para obtener el id de otra manera
+            //TODO implementar lógica para obtener el id de otra manera?
             var idUser = await _localStorage.GetItemAsync<string>(AppConstants.StorageConstants.Local.IdUser);
 
             var query = new GetAllNotificationQuery(idUser);
--- IGift/Client/Pages/Users/UserProfile.razor.cs
+++ IGift/Client/Pages/Users/UserProfile.razor.cs
@@ -1,5 +1,7 @@
-﻿using Client.Infrastructure.Authentication;
+﻿using Blazored.LocalStorage;
+using Client.Infrastructure.Authentication;
 using IGift.Client.Infrastructure.Services.Files;
+using IGift.Shared;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System.Buffers.Text;
@@ -10,13 +12,13 @@ namespace IGift.Client.Pages.Users
     public partial class UserProfile
     {
         [Inject] private IProfilePicture _profileService { get; set; }
+        [Inject] private ILocalStorageService _localStorage { get; set; }
 
         private string Nombre { get; set; }
         private string Apellido { get; set; }
-        private string Iniciales { get; set; }//TODO implementar en la foto de perfil si esta vacía
-        //private string imagenBase64 = "data:image/jpg;base64,/";
-        private string imageBase64 = string.Empty;
+        private string Iniciales { get; set; }
 
+        private string imageBase64 = string.Empty;
 
         private string background = string.Empty;
 
@@ -27,33 +29,40 @@ namespace IGift.Client.Pages.Users
 
         protected override async Task OnInitializedAsync()
         {
-            var result = await _profileService.GetByIdAsync("e16eb4a2-96a2-4f57-87b9-ea9dca746ad8");
-
-            imageBase64 = Convert.ToBase64String(result.Data.Data);
-
-            background = $"width: 80px; height: 60px; background-image: url('data:image/jpg;base64,{imageBase64}'); background-size: cover;";
-          
-
             var state = await ((IGiftAuthenticationStateProvider)_authenticationStateProvider!).GetAuthenticationStateAsync();
             var user = state.User;
             if (state != null && user.Identity.IsAuthenticated)
             {
                 Nombre = user.FindFirst(c => c.Type == ClaimTypes.Name)?.Value!;
-
                 Apellido = user.FindFirst(c => c.Type == ClaimTypes.Surname)?.Value!;
-
-                Iniciales = Nombre.Substring(0, 1).ToUpper() + Apellido.Substring(0, 1).ToUpper();
-
                 Correo = user.FindFirst(c => c.Type == ClaimTypes.Email)?.Value!;
             }
 
+            await GetProfilePicture();
         }
-
 
         private void UploadFiles(IBrowserFile file)
         {
             _files.Add(file);
             //TODO upload the files to the server
+        }
+
+        private async Task GetProfilePicture()
+        {
+            var idUser = await _localStorage.GetItemAsync<string>(AppConstants.StorageConstants.Local.IdUser);
+
+            var result = await _profileService.GetByIdAsync(idUser!);
+
+            if (!result.Succeeded)
+            {
+                imageBase64 = Convert.ToBase64String(result.Data.Data);
+
+                background = $"width: 80px; height: 60px; background-image: url('data:image/jpg;base64,{imageBase64}'); background-size: cover;";
+            }
+            else
+            {
+                Iniciales = Nombre.Substring(0, 1).ToUpper() + Apellido.Substring(0, 1).ToUpper();
+            }
         }
     }
 }
