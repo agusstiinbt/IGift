7525a9d7c9a2c972474bf49fc0995eecbd9b326d
Aesposito <agusstiinbt@gmail.com>
2024-07-29 13:53:52 -0300

    Ahora tenemos la foto de perfil también en el chat

--- IGift/Client/Layouts/Chat/ToolBar/ChatProfileToggle.razor.cs
+++ IGift/Client/Layouts/Chat/ToolBar/ChatProfileToggle.razor.cs
@@ -1,17 +1,46 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Client.Infrastructure.Authentication;
+using System.Security.Claims;
 
 namespace IGift.Client.Layouts.Chat.ToolBar
 {
     public partial class ChatProfileToggle
     {
-        [CascadingParameter]
-        public string IdUser { get; set; } = string.Empty;
+        private string IdUser { get; set; } = string.Empty;
+        private string Nombre { get; set; }
+        private string Apellido { get; set; }
+        private string Iniciales { get; set; }
 
-        protected override async Task OnParametersSetAsync()
+        private string imageBase64 = string.Empty;
+
+        private string background = string.Empty;
+        private string Correo { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
-            if (!string.IsNullOrEmpty(IdUser))
+            var state = await ((IGiftAuthenticationStateProvider)_authenticationStateProvider!).GetAuthenticationStateAsync();
+            var user = state.User;
+            if (state != null && user.Identity.IsAuthenticated)
             {
-                var result = await _profileService.GetByIdAsync(IdUser);
+                Nombre = user.FindFirst(c => c.Type == ClaimTypes.Name)?.Value!;
+                Apellido = user.FindFirst(c => c.Type == ClaimTypes.Surname)?.Value!;
+                Correo = user.FindFirst(c => c.Type == ClaimTypes.Email)?.Value!;
+                IdUser = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value!;
+            }
+            await GetProfilePicture();
+        }
+
+        private async Task GetProfilePicture()
+        {
+            var result = await _profileService.GetByIdAsync(IdUser!);
+
+            if (result.Succeeded)
+            {
+                imageBase64 = Convert.ToBase64String(result.Data.Data);
+                background = $"width: 80px; height: 60px; background-image: url('data:image/jpg;base64,{imageBase64}'); background-size: cover;";
+            }
+            else
+            {
+                Iniciales = Nombre.Substring(0, 1).ToUpper() + Apellido.Substring(0, 1).ToUpper();
             }
         }
     }
