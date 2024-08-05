8495566ab71b93343bbeb821181f22ed6e09e7ff
Aesposito <agusstiinbt@gmail.com>
2024-08-05 13:38:58 -0300

    .

---
+++ IGift/Server/Hubs/SignalRHub.cs
@@ -0,0 +1,31 @@
+﻿using IGift.Application.Models.Chat;
+using IGift.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace IGift.Server.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        public async Task OnConnectAsync(string userId)
+        {
+            await Clients.All.SendAsync(AppConstants.SignalR.ConnectUser, userId);
+        }
+
+        public async Task OnDisconnectAsync(string userId)
+        {
+            await Clients.All.SendAsync(AppConstants.SignalR.DisconnectUser, userId);
+        }
+
+        public async Task ChatNotificationAsync(string message, string receiverUserId, string senderUserId)
+        {
+            await Clients.User(receiverUserId).SendAsync(AppConstants.SignalR.ReceiveChatNotification, message, receiverUserId, senderUserId);
+        }
+
+        //public async Task SendMessageAsync(Chat chat, string userName)
+        //{
+        //    //await Clients.All.SendAsync("ReceiveMessage", userName);
+        //    await Clients.User(chat.ToUserId).SendAsync(AppConstants.SignalR.ReceiveMessage, chat, userName);
+        //    await Clients.User(chat.FromUserId).SendAsync(AppConstants.SignalR.ReceiveMessage, chat, userName);
+        //}
+    }
+}
