b8bfda881308eeae846e2e486693852ab396c060
Aesposito <agusstiinbt@gmail.com>
2024-11-12 19:03:11 -0300

    nuevo server

---
+++ IGIFT.Server.LocalesAdheridos/Controllers/LocalesAdheridosController.cs
@@ -0,0 +1,17 @@
+﻿using IGift.Application.Requests.LocalesAdheridos.Query;
+using IGIFT.Server.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IGIFT.Server.LocalesAdheridos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocalesAdheridosController : BaseApiController<LocalesAdheridosController>
+    {
+        [HttpPost]
+        public async Task<ActionResult> GetAll(GetAllLocalAdheridoQuery query)
+        {
+            return Ok(await _mediator.Send(query));
+        }
+    }
+}
---
+++ IGIFT.Server.LocalesAdheridos/Program.cs
@@ -0,0 +1,52 @@
+using IGift.Application.Interfaces.Files;
+using IGift.Application.Interfaces.Repositories;
+using IGift.Application.Requests.LocalesAdheridos.Query;
+using IGift.Infrastructure.Data;
+using IGift.Infrastructure.Repositories;
+using IGift.Infrastructure.Services.Files;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Add services to the container.
+builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllLocalAdheridoQuery).Assembly));
+
+//Mapeo
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddTransient<IUploadService, UploadService>();
+
+
+//Repositories
+builder.Services.AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+builder.Services.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
