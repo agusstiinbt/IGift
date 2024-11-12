757006aa92a4fb5d332041757245736c81201f38
Aesposito <agusstiinbt@gmail.com>
2024-11-12 19:02:31 -0300

    Creamos un proyecto compartido (shared) para todos los servicios. Creamos una clase startup y un configure services

---
+++ IGIFT.Server.Shared/IGIFT.Server.Shared/SharedConfigureServices.cs
@@ -0,0 +1,26 @@
+﻿using IGift.Application.Interfaces.Files;
+using IGift.Application.Interfaces.Repositories;
+using IGift.Application.Requests.LocalesAdheridos.Query;
+using IGift.Infrastructure.Repositories;
+using IGift.Infrastructure.Services.Files;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IGIFT.Server.Shared
+{
+    public static class SharedConfigureServices
+    {
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Configuración común de servicios
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllLocalAdheridoQuery).Assembly));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            services.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
+            services.AddTransient<IUploadService, UploadService>();
+            services.AddSwaggerGen();
+
+        }
+
+    }
+}
---
+++ IGIFT.Server.Shared/IGIFT.Server.Shared/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+namespace IGIFT.Server.Shared
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddFor
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IStringLocalizer<Startup> localizer)
+        {
+
+        }
+    }
+}
