1b1b97b357f780ca03196440f5a379f0dbf12f0b
Aesposito <agusstiinbt@gmail.com>
2025-01-01 09:54:22 -0300

    Nuevo Repositorio

---
+++ IGift.Infrastructure/Repositories/Repository2.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using IGift.Application.Interfaces.Repositories;
+using IGift.Domain.Contracts;
+using IGift.Infrastructure.Data;
+
+namespace IGift.Infrastructure.Repositories
+{
+    public class Repository2<T, TId> : IRepository2<T, TId> where T : Entity<TId>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public Repository2(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IQueryable<T> query => _context.Set<T>();
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Task> DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();//TODO fijarse si usar o no el cache remove que tiene blazorHero en esta clase en todos los metodos
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await Task.FromResult(_context.Set<T>().AsEnumerable());
+        }
+
+        public async Task<T> GetByIdAsync(TId id)
+        {
+            return await _context!.Set<T>().FindAsync(id);
+        }
+
+        public Task<IEnumerable<T>> GetPagedResponseAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await Task.FromResult(_context.Set<T>().Where(predicate).AsEnumerable());
+        }
+        public async Task<IQueryable<TDto>> FindByQuery<TDto>() where TDto : class
+        {
+            return query.ProjectTo<TDto>(_mapper.ConfigurationProvider);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            T exist = _context.Set<T>().Find(entity.Id)!;
+            _context.Entry(exist).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
--- IGift.Infrastructure/Repositories/UnitOfWork.cs
+++ IGift.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections;
-using IGift.Application.Interfaces.Repositories;
 using IGift.Application.Interfaces.Repositories.Generic.Auditable;
-using IGift.Application.Interfaces.Repositories.Generic.NonAuditable;
 using IGift.Domain.Contracts;
 using IGift.Infrastructure.Data;
 using IGift.Shared.Wrapper;
@@ -79,103 +77,10 @@ namespace IGift.Infrastructure.Repositories
                 var repositoryType = typeof(Repository<,>);
 
                 var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T), typeof(TId)), _context);
-
                 _repositories.Add(type, repositoryInstance);
+                return (IRepository<T, TId>)_repositories[type];
             }
-
-            return (IRepository<T, TId>)_repositories[type];
-        }
-
-        public Task Rollback()
-        {
-            _context.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
-            return Task.CompletedTask;
-        }
-    }
-    public class UnitOfWork2<TId> : IUnitOfWork2<TId>
-    {
-        private readonly ApplicationDbContext _context;
-        private Hashtable _repositories;
-        private bool disposed;
-        private readonly IAppCache _cache;
-
-
-        public UnitOfWork2(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IResult> Commit(string mensajeExito, CancellationToken cancellationToken)
-        {
-            int affectedRows = await _context.SaveChangesAsync(cancellationToken);
-
-            if (affectedRows > 0)
-            {
-                return await Result.SuccessAsync(mensajeExito);
-            }
-            else
-            {
-                return await Result.FailAsync("No changes were made to the database.");
-            }
-        }
-
-        public async Task<int> CommitAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys)
-        {
-            var result = await _context.SaveChangesAsync(cancellationToken);
-            foreach (var cacheKey in cacheKeys)
-            {
-                _cache.Remove(cacheKey);
-            }
-            return result;
-        }
-
-        //TODO averiguar quién está llamando al dispose y cómo lo hace
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                {
-                    //dispose managed resources
-                    _context.Dispose();
-                }
-            }
-            //dispose unmanaged resources
-            disposed = true;
-        }
-
-        public IRepository2<T, TId> Repository<T>() where T : Entity<TId>
-        {
-            if (_repositories == null)
-                _repositories = new Hashtable();
-
-            var type = typeof(T).Name;
-
-            try
-            {
-                if (!_repositories.ContainsKey(type))
-                {
-                    var repositoryType = typeof(Repository2<,>);
-
-                    var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T), typeof(TId)), _context);
-
-                    _repositories.Add(type, repositoryInstance);
-                }
-            }
-            catch (Exception e)
-            {
-
-                throw;
-            }
-
-
-            return (IRepository2<T, TId>)_repositories[type];
+            return null;
         }
 
         public Task Rollback()
