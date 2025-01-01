26d626b6207f85d9c9e08103652f1e5b5eea01df
Aesposito <agusstiinbt@gmail.com>
2025-01-01 12:13:51 -0300

    Categoria service

---
+++ Client.Infrastructure/Services/Categoria/CategoriaService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using Client.Infrastructure.Extensions;
+using IGift.Application.CQRS.Categoria.Query;
+using IGift.Application.Responses.Categoria;
+using IGift.Shared.Constants.Controllers;
+using IGift.Shared.Wrapper;
+
+namespace IGift.Client.Infrastructure.Services.Categoria
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoriaService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IResult<IEnumerable<CategoriaResponse>>> GetAll()
+        {
+            var query = new GetAllCategoriaQuery();
+
+            var url = ConstCategoriaController.GetAll;
+
+            var response = await _httpClient.PostAsJsonAsync(url, query);
+
+            return await response.ToResult<IEnumerable<CategoriaResponse>>();
+        }
+    }
+}
---
+++ Client.Infrastructure/Services/Categoria/ICategoriaService.cs
@@ -0,0 +1,10 @@
+﻿using IGift.Application.Responses.Categoria;
+using IGift.Shared.Wrapper;
+
+namespace IGift.Client.Infrastructure.Services.Categoria
+{
+    public interface ICategoriaService
+    {
+        Task<IResult<IEnumerable<CategoriaResponse>>> GetAll();
+    }
+}
