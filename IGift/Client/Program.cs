6330c14922653c11cd699331034a930ee4ea3f7e
Aesposito <agusstiinbt@gmail.com>
2025-01-01 12:14:25 -0300

    Traemos desdes el servidor las categorias

--- IGift/Client/Program.cs
+++ IGift/Client/Program.cs
@@ -4,5 +4,3 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args).AddRootComponents().AddClientServices();
 
 await builder.Build().RunAsync();
-
-
--- IGift/Client/Shared/PopOverCategorias/PopOverCategorias.razor.cs
+++ IGift/Client/Shared/PopOverCategorias/PopOverCategorias.razor.cs
@@ -1,6 +1,25 @@
-﻿namespace IGift.Client.Shared.PopOverCategorias
+﻿using IGift.Application.Responses.Categoria;
+using IGift.Client.Infrastructure.Services.Categoria;
+using Microsoft.AspNetCore.Components;
+
+namespace IGift.Client.Shared.PopOverCategorias
 {
     public partial class PopOverCategorias
     {
+        [Inject] ICategoriaService _categoriaService { get; set; }
+
+        private bool _isPopoverOpen = false;
+
+        private void ShowPopover() =>
+            _isPopoverOpen = !_isPopoverOpen;
+
+        private List<CategoriaResponse> categoriaList = new List<CategoriaResponse>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await _categoriaService.GetAll();
+            if (response.Succeeded)
+                categoriaList = response.Data.ToList();
+        }
     }
 }
