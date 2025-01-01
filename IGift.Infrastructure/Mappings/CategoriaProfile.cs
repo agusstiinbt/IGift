0bd155af31c40a4592399f1abbb6faf2b2172d2e
Aesposito <agusstiinbt@gmail.com>
2025-01-01 12:16:00 -0300

    nuevo profile de mapping

---
+++ IGift.Infrastructure/Mappings/CategoriaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using IGift.Application.Models;
+using IGift.Application.Responses.Categoria;
+
+namespace IGift.Infrastructure.Mappings
+{
+    public class CategoriaProfile : Profile
+    {
+        public CategoriaProfile()
+        {
+            CreateMap<Categoria, CategoriaResponse>();
+        }
+    }
+}
