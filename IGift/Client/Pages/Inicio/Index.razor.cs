e46844481c449e9b25dc6a31332d549d05ac3924
Aesposito <agusstiinbt@gmail.com>
2025-02-14 17:52:48 -0300

    AHora etenemos en el index.razor la logica para desloguear si se llegasaze a estar evncido el token al cargar la solucion

--- IGift/Client/Pages/Inicio/Index.razor.cs
+++ IGift/Client/Pages/Inicio/Index.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Net.Http.Headers;
+using System.Security.Claims;
 using Client.Infrastructure.Authentication;
 using IGift.Application.CQRS.Peticiones.Query;
 using IGift.Application.Responses.Peticiones;
@@ -6,19 +7,24 @@ using IGift.Client.Extensions;
 using IGift.Shared.Constants;
 using IGift.Shared.Wrapper;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.JSInterop;
 using MudBlazor;
 
 namespace IGift.Client.Pages.Inicio
 {
     public partial class Index
     {
-
         // Parametros
+        [CascadingParameter] public Task<AuthenticationState>? AuthenticationState { get; set; }
         [Parameter] public string? _Categoria { get; set; }
         [Parameter] public string? TxtBusqueda { get; set; } = string.Empty;
         [Parameter] public PaginatedResult<PeticionesResponse>? _datosDeBusqueda { get; set; } = null;
-        [CascadingParameter] public HubConnection? _hubConnection { get; set; }
+
+        public HubConnection? _hubConnection { get; set; }
+
+        private string? CurrentUserId { get; set; }
 
         //Propiedades
         private PaginatedResult<PeticionesResponse>? peticiones { get; set; } = null;
@@ -42,21 +48,18 @@ namespace IGift.Client.Pages.Inicio
 
         protected override async Task OnInitializedAsync()
         {
-            try
+            var authState = await AuthenticationState;
+            if (authState.User.Identity!.IsAuthenticated)
             {
-                if (_hubConnection == null)
-                    _hubConnection = await _hubConnection.TryInitialize(_nav, _authService, _localStorage);
-            }
-            catch (Exception e)
-            {
-                _snack.Add(e.Message);
-            }
+                _interceptor.RegisterEvent();
+                await InitializeHub();
+                await _authService.Disconnect(DotNetObjectReference.Create(this));
 
-            var state = await ((IGiftAuthenticationStateProvider)_authenticationStateProvider!).GetAuthenticationStateAsync();
+                var state = await ((IGiftAuthenticationStateProvider)_authenticationStateProvider!).GetAuthenticationStateAsync();
 
-            NombreUsuario = state.User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value!;
+                NombreUsuario = state.User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value!;
+            }
         }
-
 
         private void SeleccionarBoton(string boton)
         {
@@ -132,6 +135,142 @@ namespace IGift.Client.Pages.Inicio
         {
             TxtBusqueda = text;
             await _table.ReloadServerData();
+        }
+
+        /// <summary>
+        /// Inicializamos todas las conexiones de tipo Hub
+        /// </summary>
+        /// <returns></returns>
+        private async Task InitializeHub()
+        {
+            try
+            {
+                _hubConnection = await _hubConnection.TryInitialize(_nav, _authService, _localStorage);
+                if (_hubConnection != null)
+                {
+                    _hubConnection.On<string, string, string>(AppConstants.SignalR.ReceiveChatNotification, (message, receiverUserId, senderUserId) =>
+                    {
+                        if (CurrentUserId == receiverUserId)
+                        {
+                            //TODO implementar:..._jsRuntime.InvokeAsync<string>("PlayAudio", "notification");
+                            _snack.Add(message, Severity.Info, config =>
+                            {
+                                config.VisibleStateDuration = 10000;
+                                config.HideTransitionDuration = 500;
+                                config.ShowTransitionDuration = 500;
+                                config.Action = "Chat?";
+                                config.ActionColor = Color.Primary;
+                                config.Onclick = snackbar =>
+                                {
+                                    _nav.NavigateTo($"chat/{senderUserId}");
+                                    return Task.CompletedTask;
+                                };
+                            });
+                        }
+                    });
+
+                    _hubConnection.On(AppConstants.SignalR.ReceiveRegenerateTokens, async () =>
+                    {
+                        try
+                        {
+                            var token = await _authService.TryForceRefreshToken();
+                            if (!string.IsNullOrEmpty(token))
+                            {
+                                _snack.Add("Token refrescado", Severity.Success);
+                                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            _snack.Add("Sesión finalizada", Severity.Error);
+                            await _authService.Logout();
+                            _nav.NavigateTo("/");
+                        }
+                    });
+
+                    //TODO investigar sobre esto porque puede ser util para desloguer automaticamente cuando se le haya cambiado permisos o roles a un usuario
+                    //_hubConnection.On<string, string>(AppConstants.SignalR.LogoutUsersByRole, async (userId, roleId) =>
+                    //{
+                    //    if (currentUserId != userId)
+                    //    {
+                    //        var rolesResponse = await RoleManager.GetRolesAsync();
+                    //        if (rolesResponse.Succeeded)
+                    //        {
+                    //            var role = rolesResponse.Data.FirstOrDefault(x => x.Id == roleId);
+                    //            if (role != null)
+                    //            {
+                    //                var currentUserRolesResponse = await _userManager.GetRolesAsync(CurrentUserId);
+                    //                if (currentUserRolesResponse.Succeeded && currentUserRolesResponse.Data.UserRoles.Any(x => x.RoleName == role.Name))
+                    //                {
+                    //                    _snackBar.Add(_localizer["You are logged out because the Permissions of one of your Roles have been updated."], Severity.Error);
+                    //                    await hubConnection.SendAsync(ApplicationConstants.SignalR.OnDisconnect, CurrentUserId);
+                    //                    await _authenticationManager.Logout();
+                    //                    _navigationManager.NavigateTo("/login");
+                    //                }
+                    //            }
+                    //        }
+                    //    }
+                    //});
+
+                    _hubConnection.On<string>(AppConstants.SignalR.PingRequest, async (userName) =>
+                    {
+                        await _hubConnection.SendAsync(AppConstants.SignalR.PingResponse, CurrentUserId, userName);
+                    });
+
+                    CurrentUserId = AuthenticationState.Result.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value!;
+
+                    await _hubConnection.SendAsync(AppConstants.SignalR.OnConnect, CurrentUserId);
+
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("401"))
+                {
+                    _snack.Add("Conexion con SignalR perdida. Proceda con cuidado", Severity.Info, config =>
+                    {
+                        config.VisibleStateDuration = 3000;
+                        config.HideTransitionDuration = 500;
+                        config.ShowTransitionDuration = 500;
+                        config.ActionColor = Color.Primary;
+                    });
+                }
+                else
+                    _snack.Add(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Este método es invocado por javascript en el caso de que se haya excedido el tiempo de inactividad y nos deslogueamos
+        /// </summary>
+        /// <returns></returns>
+        [JSInvokable]
+        public async Task Logout()
+        {
+            var authState = await AuthenticationState;
+            if (authState.User.Identity!.IsAuthenticated)
+            {
+                _nav.NavigateTo(AppConstants.Routes.Logout);
+            }
+        }
+
+        /// <summary>
+        /// Este mensaje es invocado por javascript en el caso de que tengamos tiempo de inactividad
+        /// </summary>
+        /// <returns></returns>
+        [JSInvokable]
+        public async Task ShowMessage()
+        {
+            var authState = await AuthenticationState;
+            if (authState.User.Identity!.IsAuthenticated)
+            {
+                _snack.Add("Su sesión está por expirar por inactividad", Severity.Warning);
+            }
+        }
+
+        public void Dispose()
+        {
+            _interceptor.DisposeEvent();
         }
     }
 }
