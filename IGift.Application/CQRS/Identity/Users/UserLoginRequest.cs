699e10a502a510ed8b8294c57b29ee19b62b7d3d
Aesposito <agusstiinbt@gmail.com>
2025-02-15 18:28:13 -0300

    Agregamos validaciones para Data Anotations

--- IGift.Application/CQRS/Identity/Users/UserLoginRequest.cs
+++ IGift.Application/CQRS/Identity/Users/UserLoginRequest.cs
@@ -4,11 +4,11 @@ namespace IGift.Application.CQRS.Identity.Users
 {
     public class UserLoginRequest
     {
-        [EmailAddress]
-        [Required]
+        [EmailAddress(ErrorMessage = "El formato de correo es incorrecto.")]
+        [Required(ErrorMessage = "El correo es necesario.")]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Por favor, ingrese una contraseña.")]
         [DataType(DataType.Password)]
         public string? Password { get; set; }
     }
